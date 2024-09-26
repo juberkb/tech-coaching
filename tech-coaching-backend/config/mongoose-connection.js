6991ad26d99330d1c9b319ad2149460fb69c7f1d
unknown <juberkhankb982@gmail.com>
2024-09-26 18:02:33 +0530

    commit from office at 26 sept 2024

--- tech-coaching-backend/config/mongoose-connection.js
+++ tech-coaching-backend/config/mongoose-connection.js
@@ -1,19 +1,33 @@
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+// const dotenv = require('dotenv');
+const config=require("config");
 
-dotenv.config();
-
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log('MongoDB connected');
-  } catch (error) {
-    console.error('Error connecting to MongoDB:', error);
-    process.exit(1);
+const dbgr =require("debug") ("development:mongoooseose");  //only name you can whatever you want("development:monggose") 
+// dotenv.config();
+mongoose
+.connect(`${config.get("MONGODB_URI")}/tech-coaching-backend`)
+.then(
+  function(){
+    dbgr("conected")
+    console.log("MongoDB connected successfully");  // Fallback to ensure output
   }
-};
+)
+.catch(function(error){
+  dbgr(error)
+  console.log("error connecting MongoDB ");  // Fallback to ensure output
+})
 
-module.exports = connectDB;
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGODB_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log('MongoDB connected');
+//   } catch (error) {
+//     console.error('Error connecting to MongoDB:', error);
+//     process.exit(1);
+//   }
+// };
+
+// module.exports = connectDB;
